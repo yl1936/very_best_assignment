649752b0113afa5c3441ec63d4bfece3e2b7800b
anonymous anonymous <autumn.lin@kellogg.northwestern.edu>
2022-02-21 08:46:01 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,13 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :email, :string
+  attribute :password, :string
+  attribute :username, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
